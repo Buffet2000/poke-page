119a7355cb50dc082de8f90eb32dd6d1957d1711
Yury Fedulov <buffet2000@gmail.com>
2024-08-19 22:57:48 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  import "../app.css";
+  import { goto } from "$app/navigation";
+</script>
+
+<!-- Navbar -->
+<div class="bg-base-100 justify-between fixed top-0 left-0 right-0">
+  <div class="navbar max-w-3xl mx-auto justify-between">
+    <div>
+      <a href="/" class="btn btn-ghost text-xl">PokePage</a>
+      <a href="/user" class="btn btn-ghost">PokePage</a>
+    </div>
+    <div>
+      <button on:click-{() => goto('/login')}></button>
+    </div>
+  </div>
+</div>
+
+<slot></slot>
