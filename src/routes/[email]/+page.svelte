e8e3f9de776d634d5ea60ca4466ae5f17f9d9750
Yury Fedulov <buffet2000@gmail.com>
2024-08-24 10:40:56 +0300

    feat: added API for pokemons

--- src/routes/[email]/+page.svelte
+++ src/routes/[email]/+page.svelte
@@ -1,8 +1,38 @@
 <!-- localmost:5137/[email] -->
 <script lang="ts">
   import { page } from '$app/stores';
+  import { getPokemonList, getPokemon } from '$lib/pokemonAPI';
 
   $: email = $page.params.email;
+
+  let pokemonList: any = []
+  let pokemonData: any = []
+  let profile: any = {
+    description: 'This is ypur description, feel free to write about yourself or your favorite Pokemon.',
+    pokemon_ids: [1, 2, 5]
+  }
+
+  async function refreshPokemonData() {
+    pokemonData = [];
+
+    profile.pokemon_ids.map(async (id: number) => {
+      const data = await getPokemon(id.toString());
+
+      if (pokemonData === undefined) {
+        pokemonData = [data];
+      } else {
+        pokemonData = [ ...pokemonData, data ]
+      }
+    })
+  }
+
+  page.subscribe(async () => {
+    pokemonList = await getPokemonList();
+    await refreshPokemonData();
+    console.log(await getPokemon('1'));
+    console.log(pokemonList);
+    console.log(pokemonData);
+  })
 </script>
 
 <div class="hero min-h-screen bg-base-300">
@@ -11,26 +41,21 @@
       <h1 class="text-white font-bold text-4xl">{email}'s Page</h1>
       <p class="py-3 max-w-md mx-auto">User description here...</p>
       <div class="grid grid-cols-3">
-        <div class="card bg-slate-700 m-4 shadow-lg shadow-blue-900">
-          <div class="card-body">
-            <div class="text-center">
-              <!-- svelte-ignore a11y-img-redundant-alt -->
-              <img src={"#"} alt="Pokemon Image" class="w-32 h-32 mx-auto bg-gray-500" />
-              <h2 class="text-white text-2xl font-bold">Pikachu</h2>
-              <p class="text-info">Electric</p>
+        {#if pokemonData === undefined}
+          <p>Loading...</p>
+        {:else}
+          {#each pokemonData as pokemon}
+            <div class="card bg-slate-700 m-4 shadow-lg shadow-blue-900">
+              <div class="card-body">
+                <div class="text-center">
+                  <img src={pokemon.sprites.front_default} alt="Pokemon" class="w-32 h-32 mx-auto" />
+                  <h2 class="text-white text-2xl font-bold">{pokemon.name}</h2>
+                  <p class="text-info">{pokemon.types[0].type.name}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="card bg-slate-700 m-4 shadow-lg shadow-blue-900">
-          <div class="card-body">
-            <div class="text-centert">
-              <!-- svelte-ignore a11y-img-redundant-alt -->
-              <img src={"#"} alt="Pokemon Image" class="w-32 h-32 mx-auto bg-gray-500" />
-              <h2 class="text-white text-2xl font-bold">Pikachu</h2>
-              <p class="text-info">Electric</p>
-            </div>
-          </div>
-        </div>
+          {/each}
+        {/if}
       </div>
     </div>
   </div>
