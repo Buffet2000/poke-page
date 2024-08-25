ec6b6f84860985741cd4215fd012d71e2b84cb58
Yury Fedulov <buffet2000@gmail.com>
2024-08-25 13:15:17 +0300

    docs

---
+++ docs/_app/env.js
@@ -0,0 +1,1 @@
+export const env={"PUBLIC_SUPABASE_URL":"https://dnzjeefemnkbbbvaufqa.supabase.co","PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuemplZWZlbW5rYmJidmF1ZnFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwOTUyMzIsImV4cCI6MjAzOTY3MTIzMn0.Mom5-3wJ9RsHliMoLrrpsy0magBpOccw5OAgLpL8I5E"}
---
+++ docs/_app/immutable/entry/start.RUwtY8eJ.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CL43PwIq.js";export{t as start};
